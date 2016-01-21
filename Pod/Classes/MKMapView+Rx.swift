3adb82b673747ea459cc869af655bfd82475ce8c
Thane Gill <me@thanegill.com>
2016-01-21 11:48:14 -0800

    Remove mapView prefix from rx_ methods

--- Pod/Classes/MKMapView+Rx.swift
+++ Pod/Classes/MKMapView+Rx.swift
@@ -39,21 +39,21 @@ extension MKMapView {
 
     // MARK: Loading the Map Data
 
-    public var rx_mapViewWillStartLoadingMap: Observable<Void>{
+    public var rx_willStartLoadingMap: Observable<Void>{
         return rx_delegate.observe("mapViewWillStartLoadingMap:")
             .map { _ in
                 return()
         }
     }
 
-    public var rx_mapViewDidFinishLoadingMap: Observable<Void>{
+    public var rx_didFinishLoadingMap: Observable<Void>{
         return rx_delegate.observe("mapViewDidFinishLoadingMap:")
             .map { _ in
                 return()
         }
     }
 
-    public var rx_mapViewDidFailLoadingMap: Observable<NSError!>{
+    public var rx_didFailLoadingMap: Observable<NSError!>{
         return rx_delegate.observe("mapViewDidFailLoadingMap:withError:")
             .map { a in
                 return a[1] as? NSError
@@ -62,14 +62,14 @@ extension MKMapView {
 
     // MARK: Responding to Rendering Events
 
-    public var rx_mapViewWillStartRenderingMap: Observable<Void>{
+    public var rx_willStartRenderingMap: Observable<Void>{
         return rx_delegate.observe("mapViewWillStartRenderingMap:")
             .map { _ in
                 return()
         }
     }
 
-    public var rx_mapViewDidFinishRenderingMap: Observable<Bool!> {
+    public var rx_didFinishRenderingMap: Observable<Bool!> {
         return rx_delegate.observe("mapViewDidFinishRenderingMap:fullyRendered:")
             .map { a in
                 return a[1] as? Bool
@@ -78,28 +78,28 @@ extension MKMapView {
 
     // MARK: Tracking the User Location
 
-    public var rx_mapViewWillStartLocatingUser: Observable<Void> {
+    public var rx_willStartLocatingUser: Observable<Void> {
         return rx_delegate.observe("mapViewWillStartLocatingUser:")
             .map { _ in
                 return()
         }
     }
 
-    public var rx_mapViewDidStopLocatingUser: Observable<Void> {
+    public var rx_didStopLocatingUser: Observable<Void> {
         return rx_delegate.observe("mapViewDidStopLocatingUser:")
             .map { _ in
                 return()
         }
     }
 
-    public var rx_mapViewDidUpdateUserLocation: Observable<MKUserLocation!> {
+    public var rx_didUpdateUserLocation: Observable<MKUserLocation!> {
         return rx_delegate.observe("mapView:didUpdateUserLocation:")
             .map { a in
                 return a[1] as? MKUserLocation
         }
     }
 
-    public var rx_mapViewDidFailToLocateUserWithError: Observable<NSError!> {
+    public var rx_didFailToLocateUserWithError: Observable<NSError!> {
         return rx_delegate.observe("mapView:didFailToLocateUserWithError:")
             .map { a in
                 return a[1] as? NSError
@@ -108,14 +108,14 @@ extension MKMapView {
 
     // MARK: Responding to Annotation Views
 
-    public var rx_mapViewDidAddAnnotationViews: Observable<[MKAnnotationView]!> {
+    public var rx_didAddAnnotationViews: Observable<[MKAnnotationView]!> {
         return rx_delegate.observe("mapView:didAddAnnotationViews:")
             .map { a in
                 return a[1] as? [MKAnnotationView]
         }
     }
 
-    public var rx_mapViewAnnotationViewCalloutAccessoryControlTapped: Observable<Void> {
+    public var rx_annotationViewCalloutAccessoryControlTapped: Observable<Void> {
         return rx_delegate.observe("mapView:annotationView:calloutAccessoryControlTapped:")
             .map { _ in
                 return()
@@ -124,21 +124,21 @@ extension MKMapView {
 
     // MARK: Selecting Annotation Views
 
-    public var rx_mapViewDidSelectAnnotationView: Observable<MKAnnotationView!> {
+    public var rx_didSelectAnnotationView: Observable<MKAnnotationView!> {
         return rx_delegate.observe("mapView:didSelectAnnotationView:")
             .map { a in
                 return a[1] as? MKAnnotationView
         }
     }
 
-    public var rx_mapViewDidDeselectAnnotationView: Observable<MKAnnotationView!> {
+    public var rx_didDeselectAnnotationView: Observable<MKAnnotationView!> {
         return rx_delegate.observe("mapView:didDeselectAnnotationView:")
             .map { a in
                 return a[1] as? MKAnnotationView
         }
     }
 
-    public var rx_mapViewDidChangeState: Observable<(view: MKAnnotationView!, newState: MKAnnotationViewDragState, oldState: MKAnnotationViewDragState)> {
+    public var rx_didChangeState: Observable<(view: MKAnnotationView!, newState: MKAnnotationViewDragState, oldState: MKAnnotationViewDragState)> {
         return rx_delegate.observe("mapView:annotationView:didChangeDragState:fromOldState:")
             .map { a in
                 return (view: a[1] as? MKAnnotationView, newState: a[2] as! MKAnnotationViewDragState, oldState: a[3] as! MKAnnotationViewDragState)
