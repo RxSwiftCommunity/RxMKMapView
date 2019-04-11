a89e59859e46a7ffcbd37fbcbfdcda3868b3e130
Mohamed Louli <mohamed.louli@immoweb.be>
2019-04-11 15:49:39 +0200

    Update to Swift 5.0

--- Sources/Diff.swift
+++ Sources/Diff.swift
@@ -21,9 +21,9 @@ private struct BoxedHashable<T: NSObjectProtocol>: Hashable {
         self.value = value
     }
 
-    public var hashValue: Int {
-        return self.value.hash
-    }
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(value.hash)
+	}
 
     public static func == (lhs: BoxedHashable, rhs: BoxedHashable) -> Bool {
         return lhs.value.isEqual(rhs.value)
