ce41a3e6b2a1f75e346803b607db07b6f7973393
Greg Jurzak <greg.jurzak@mobile-5.com>
2020-02-06 04:26:02 +0100

    Add Swift Package Manager manifest file.

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Package",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "RxMKMapView",
+            targets: ["RxMKMapView"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/ReactiveX/RxSwift",
+            .exact("5.0.1")
+        )
+    ],
+    targets: [
+        .target(
+            name: "RxMKMapView",
+            dependencies: ["RxSwift", "RxCocoa"]),
+    ]
+)
